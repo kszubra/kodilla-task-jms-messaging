9b8c5af8a5369a70674d57d516c93341f2d8c8ac
Kacper Szubra <kacper.szubra@nvt.pl>
2021-10-19 18:13:34 +0200

    Data annotation and removed controller brackets

--- src/main/java/com/kodilla/task/jmsmessaging/controller/OrderController.java
+++ src/main/java/com/kodilla/task/jmsmessaging/controller/OrderController.java
@@ -18,7 +18,7 @@ import lombok.extern.slf4j.Slf4j;
 public class OrderController {
     private JmsTemplate jmsTemplate;
 
-    @PostMapping()
+    @PostMapping
     public void processOrder(@RequestBody Order order) {
         log.info("Received order: {}", order);
 
--- src/main/java/com/kodilla/task/jmsmessaging/domain/Order.java
+++ src/main/java/com/kodilla/task/jmsmessaging/domain/Order.java
@@ -3,21 +3,9 @@ package com.kodilla.task.jmsmessaging.domain;
 import java.math.BigDecimal;
 import java.util.List;
 
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.EqualsAndHashCode;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
-import lombok.ToString;
+import lombok.Data;
 
-@AllArgsConstructor
-@NoArgsConstructor
-@Getter
-@Setter
-@Builder
-@ToString
-@EqualsAndHashCode
+@Data
 public class Order {
 
     private List<String> productsIds;
