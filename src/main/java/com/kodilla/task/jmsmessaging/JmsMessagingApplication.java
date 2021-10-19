1fde8e50f175e174ea40dc56dc421b8f07ced6ac
Kacper Szubra <kacper.szubra@nvt.pl>
2021-10-19 10:15:32 +0200

    initial commit

---
+++ src/main/java/com/kodilla/task/jmsmessaging/JmsMessagingApplication.java
@@ -0,0 +1,13 @@
+package com.kodilla.task.jmsmessaging;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JmsMessagingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JmsMessagingApplication.class, args);
+    }
+
+}
