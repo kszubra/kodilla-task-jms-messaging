97b0d3a7191f357e82a83a25bcea44a36d7e35d3
Kacper Szubra <kacper.szubra@nvt.pl>
2021-10-19 19:58:23 +0200

    All and No args constructors

--- src/main/java/com/kodilla/task/jmsmessaging/domain/Order.java
+++ src/main/java/com/kodilla/task/jmsmessaging/domain/Order.java
@@ -3,8 +3,12 @@ package com.kodilla.task.jmsmessaging.domain;
 import java.math.BigDecimal;
 import java.util.List;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
+@AllArgsConstructor
+@NoArgsConstructor
 @Data
 public class Order {
 
